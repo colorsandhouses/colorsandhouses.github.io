5ec80c7f52079faee9910c2255314e4942622a5f
CodeMate <121621804+Clashroyalo5@users.noreply.github.com>
2025-05-15 19:46:04 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -78,15 +78,3 @@ if (event.target == document.getElementById('productsModal')) {
   document.getElementById('productsModal').style.display = 'none';
 }
 };
-
-let currentIndex = 0;
-const slides = document.querySelectorAll('.carousel ul li');
-const totalSlides = slides.length;
-
-function moveSlide() {
-  currentIndex = (currentIndex + 1) % totalSlides;
-  const translateValue = -currentIndex * 100 + '%';
-  document.querySelector('.carousel ul').style.transform = `translateX(${translateValue})`;
-}
-
-setInterval(moveSlide, 4500); // Cambia de imagen cada 4,5 segundos
