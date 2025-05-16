dd26939455b4e6bf7340f11fe3ffb12e21063657
CodeMate <121621804+Clashroyalo5@users.noreply.github.com>
2025-05-15 20:33:54 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -78,3 +78,28 @@ if (event.target == document.getElementById('productsModal')) {
   document.getElementById('productsModal').style.display = 'none';
 }
 };
+
+const throttle = (func, limit) => {
+  let lastFunc;
+  let lastRan;
+  return function () {
+    const context = this;
+    const args = arguments;
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(function () {
+        if (Date.now() - lastRan >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+};
+
+window.addEventListener('scroll', throttle(() => {
+  console.log('Scroll optimizado');
+}, 100));
